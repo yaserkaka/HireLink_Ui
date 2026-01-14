e0195d8392d090df18f9e8ba6d14fa9c9fc5efaa
Yasser Fawzy Mohamed <95346533+yaserkaka@users.noreply.github.com>
2026-01-14 17:12:51 +0200

    Update HotFix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+ /** @type {import('tailwindcss').Config} */
 //import flowbiteplugin from 'flowbite/plugin'
 
-export default {
-	content: [
-		"./src/**/*.{js,jsx,ts,tsx,html}",
-		"./index.html",
-		"node_modules/flowbite-react/lib/esm/**/*.js",
-	],
+ export default {
+   content: ["./src/**/*.{js,jsx,ts,tsx,html}",
+    "./index.html",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
 
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		//  flowbiteplugin,
-	],
-};
+   ],
+
+   theme: {
+     extend: {},
+   },
+   plugins: [
+    //  flowbiteplugin,
+   ],
+ }
